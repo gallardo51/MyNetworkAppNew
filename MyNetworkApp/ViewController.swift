55b87043ee9fc4d24c0f11f07f9e5c5f634fd41f
Alex Sobolev <31108@mail.ru>
2024-03-20 15:32:23 +0300

    Initial Commit

---
+++ MyNetworkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNetworkApp
+//
+//  Created by Александр Соболев on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
