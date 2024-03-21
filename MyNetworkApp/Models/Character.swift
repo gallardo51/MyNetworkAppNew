891c21da258979fd58babbf382f67b1b39082312
Alex Sobolev <31108@mail.ru>
2024-03-21 22:46:18 +0300

    Setup Model

--- MyNetworkApp/Models/Character.swift
+++ MyNetworkApp/Models/Character.swift
@@ -4,14 +4,6 @@
 //
 //  Created by Александр Соболев on 20.03.2024.
 //
-struct Course: Decodable {
-    let id: Int?
-    let name: String?
-    let link: String?
-    let imageUrl: String?
-    let number_of_lessons: Int?
-    let number_of_tests: Int?
-}
 
 struct WebsiteDescription: Decodable {
     let info: Info
@@ -26,6 +18,7 @@ struct Info: Decodable {
 }
 
 struct Character: Decodable {
+    let _id: Int?
     let films: [String]?
     let shortFilms: [String]?
     let tvShows: [String]?
@@ -33,16 +26,16 @@ struct Character: Decodable {
     let parkAttractions: [String]?
     let allies: [String]?
     let enemies: [String]?
-    let sourceURL: String?
+    let sourceUrl: String?
     let name: String?
-    let imageURL: String?
-    let url: String?
+    let imageUrl: String?
     let createdAt: String?
-    let updatedAt: String
+    let updatedAt: String?
+    let url: String?
+    let __v: Int?
 }
 
 enum Link: String {
-    case imageURL = "https://img2.akspic.ru/attachments/crops/0/4/5/6/6/166540/166540-ios_15-yabloko-ios-ios_14-sklon-1080x1920.jpg"
+    case imageURL = "https://img2.akspic.ru/attachments/crops/3/7/1/6/6/166173/166173-yabloko-ios-apple-iphone-ios_15-1080x1920.jpg"
     case charactersURL = "https://api.disneyapi.dev/character"
-    case courses = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
 }
--- MyNetworkApp/Views/CharacterCell.swift
+++ MyNetworkApp/Views/CharacterCell.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class CharacterCell: UITableViewCell {
-    @IBOutlet weak var characterImageView: UIImageView!
+    @IBOutlet weak var characterImageView: UIImageView! {
+        didSet {
+            characterImageView.contentMode = .scaleAspectFill
+            characterImageView.clipsToBounds = true
+            characterImageView.layer.cornerRadius = characterImageView.frame.height / 2
+            characterImageView.backgroundColor = .gray
+        }
+    }
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var createdLabel: UILabel!
     @IBOutlet weak var updatedLabel: UILabel!
@@ -18,7 +25,7 @@ class CharacterCell: UITableViewCell {
         createdLabel.text = character.createdAt
         updatedLabel.text = character.updatedAt
         DispatchQueue.global().async {
-            guard let url = URL(string: character.imageURL ?? "") else { return }
+            guard let url = URL(string: character.imageUrl ?? "") else { return }
             guard let imageData = try? Data(contentsOf: url) else { return }
             DispatchQueue.main.async {
                 self.characterImageView.image = UIImage(data: imageData)
